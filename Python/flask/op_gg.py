ec9666a7635325e2c49df72140ed823dbb699839
Kimtaewan <unt1013@gmail.com>
2019-12-23 09:07:29 +0900

    .

--- Python/flask/op_gg.py
+++ Python/flask/op_gg.py
@@ -20,8 +20,9 @@ def op_gg():
     userTier = data.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierRank").text
     userScore = data.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierInfo > span.LeaguePoints").text
     userWin = data.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins").text
+    friendlyUser = data.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.SummonersMostGame.Box > div.Content > table > tbody > tr:nth-child(1) > td.SummonerName.Cell.left_select_played_with_summoner > a").text
 
-    return render_template('op_gg.html', username = username, userTier = userTier, userScore = userScore, userWin = userWin)
+    return render_template('op_gg.html', username = username, userTier = userTier, userScore = userScore, userWin = userWin, friendlyUser = friendlyUser)
 
 if __name__ == "__main__" :
     app.run(debug=True)
